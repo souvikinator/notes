8b8980b83255f9f0335fdde45dfe52a3754dc420
souvikinator <souvikinator@pop-os.localdomain>
2021-05-31 14:55:43 +0530

    initial push

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { Button } from "reactstrap";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>OneNote Clone</h1>
+            <div>
+                <Button color="primary">primary</Button>{" "}
+                <Button color="secondary">secondary</Button>{" "}
+                <Button color="success">success</Button>{" "}
+                <Button color="info">info</Button>{" "}
+                <Button color="warning">warning</Button>{" "}
+                <Button color="danger">danger</Button>{" "}
+                <Button color="link">link</Button>
+            </div>
+        </div>
+    );
 }
 
 export default App;
